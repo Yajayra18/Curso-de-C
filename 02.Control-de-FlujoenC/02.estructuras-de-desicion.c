2973d17c024929b0c815f84245c5bd9bd24ed559
Yajayra <yajayra.arauco@gmail.com>
2021-09-09 19:59:00 -0500

    02.Estructuras de desicion

---
+++ 02.Control-de-FlujoenC/02.estructuras-de-desicion.c
@@ -0,0 +1,22 @@
+/*Statements: son lineas de codigo individuales*/
+/*Bloques: son bloques de codigo que están conformados por conjuntos de standments*/
+
+
+if (expresion)
+{
+    statement1
+    statement1.1
+    statement1.2
+    
+}
+
+else if(expresion)
+    statement2
+
+/*es correcto no poner llaves si solo hay un statement*/
+
+else if(expresion)
+    statement3
+
+else
+    statement4
