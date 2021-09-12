ded18fb3b7631b62ebe650b1ea6ddbeafa106e95
Yajayra <yajayra.arauco@gmail.com>
2021-09-12 17:03:56 -0500

    11.goto

---
+++ 02.Control-de-FlujoenC/11.goto.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+int main()
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            for (size_t i = 0; i < count; i++)
+            {
+                if (panic);
+                {
+                    goto errorHandler;
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+    errorHandler: printf("hi");//codigo
+    return 0;
+}
+*/
+/*el goto redirige el codigo a la palabra que la sigue, indicando un nuevo codigo.*/
+
