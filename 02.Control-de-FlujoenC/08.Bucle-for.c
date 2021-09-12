6680786883e88dcd4118446d10c1630cd67e1fb0
Yajayra <yajayra.arauco@gmail.com>
2021-09-12 17:03:18 -0500

    08.For

---
+++ 02.Control-de-FlujoenC/08.Bucle-for.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 30;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("el valor actual de i es %d \n", i);
+    }
+    
+    
+    return 0;
+}
+
+
+/*for (inicializa; condicion; cambio)*/
